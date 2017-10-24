b8a7d46eb0d7495f95300d938388779d8c266f02
yulinliu101 <yulinliu101@users.noreply.github.com>
2017-10-24 15:11:04 -0700

    Correct the fit

--- AudioSegmentation.py
+++ AudioSegmentation.py
@@ -5,23 +5,8 @@ from scipy.stats import multivariate_normal as mvn
 
 class AudioSegmentation:
     def __init__(self, 
-                 features,
-                 min_wind = 200,
-                 inc_wind = 50,
-                 max_wind = 500,
-                 mov_frame = 10,
-                 mixture = False,
-                 Lambda = 1
-                 ):
-
+                 features):
         self.features = features
-        self.min_wind = min_wind
-        self.inc_wind = inc_wind
-        self.max_wind = max_wind
-        self.mov_frame = mov_frame
-
-        self.mixture = mixture
-        self.Lambda = Lambda
 
     def __helperSeg__(self, 
                       features, 
@@ -41,7 +26,7 @@ class AudioSegmentation:
             L0 = np.sum(np.log(mvn.pdf(analFrame.T, mean = analFrame.mean(axis = 1), cov = np.cov(analFrame), allow_singular=True)))
             L0 += Lambda/2 * (d + d * (d + 1) / 2) * np.log(Nz)
         
-        for Nx in range(startFrame + incFrame, endFrame, incFrame):
+        for Nx in range(startFrame + 2 * incFrame, endFrame - incFrame, incFrame):
             features_x = features[:, startFrame:Nx]
             features_y = features[:, Nx: endFrame]
             L1_x = np.sum(mvn.logpdf(features_x.T, mean = features_x.mean(axis = 1), cov = np.cov(features_x), allow_singular=True))
@@ -52,38 +37,44 @@ class AudioSegmentation:
                 return Nx
         return -1
             
-    def Segmentation(self):
+    def Segmentation(self,
+                     min_wind = 200,
+                     inc_wind = 50,
+                     max_wind = 500,
+                     mov_frame = 10,
+                     mixture = False,
+                     Lambda = 1):
         Seg = []
         currentST = 0
-        currentET = self.min_wind
+        currentET = min_wind
         currentSize = currentET - currentST
         # initialization
-        turnPt = self.__helperSeg__(self.features, startFrame = currentST, endFrame = currentET, incFrame = self.mov_frame, mixture = self.mixture, Lambda = self.Lambda)
+        turnPt = self.__helperSeg__(self.features, startFrame = currentST, endFrame = currentET, incFrame = mov_frame, mixture = mixture, Lambda = Lambda)
         if turnPt != -1:
             currentST = turnPt
-            currentET = turnPt + self.min_wind
+            currentET = turnPt + min_wind
             currentSize = currentET - currentST
             Seg.append(turnPt)
             turnPt = -1
         while currentST < self.features.shape[1]:
-            while currentSize <= self.max_wind and turnPt == -1:
+            while currentSize <= max_wind and turnPt == -1:
     #             print(currentET)
-                currentET += self.inc_wind
+                currentET += inc_wind
                 if currentET > self.features.shape[1]:
                     currentET = self.features.shape[1]
-                    turnPt = self.__helperSeg__(self.features, startFrame = currentST, endFrame = currentET, incFrame = self.mov_frame, mixture = self.mixture, Lambda = self.Lambda)
+                    turnPt = self.__helperSeg__(self.features, startFrame = currentST, endFrame = currentET, incFrame = mov_frame, mixture = mixture, Lambda = Lambda)
                     break
                 currentSize = currentET - currentST
-                turnPt = self.__helperSeg__(self.features, startFrame = currentST, endFrame = currentET, incFrame = self.mov_frame, mixture = self.mixture, Lambda = self.Lambda)
+                turnPt = self.__helperSeg__(self.features, startFrame = currentST, endFrame = currentET, incFrame = mov_frame, mixture = mixture, Lambda = Lambda)
             
-            if currentSize > self.max_wind:
-                currentST = currentST + self.max_wind - self.min_wind
-                currentET = currentST + self.min_wind
+            if currentSize > max_wind:
+                currentST = currentST + max_wind - min_wind
+                currentET = currentST + min_wind
                 currentSize = currentET - currentST
             elif turnPt != -1:
                 Seg.append(turnPt)
                 currentST = turnPt
-                currentET = currentST + self.min_wind
+                currentET = currentST + min_wind
                 currentSize = currentET - currentST
                 turnPt = -1
             else:
