60e033b15ad02fac46401130d2e57aa5b12596d6
yulinliu101 <yulinliu101@users.noreply.github.com>
2018-06-14 16:55:07 -0400

    bug fixed: sort index

--- AudioActDet.py
+++ AudioActDet.py
@@ -7,8 +7,9 @@
 from __future__ import division
 import os
 import numpy as np
-from itertools import groupby, chain
+from itertools import groupby, count
 from operator import itemgetter
+import matplotlib.pyplot as plt
 
 class AudioActDet:
     def __init__(self, AudioFeatures):
@@ -28,9 +29,21 @@ class AudioActDet:
         # sec_to_bin is usally bin.shape[0] / audio length(ms)/1000
         
         idx_range = []
-        for k, g in groupby(enumerate(np.where(power <= power_threshold)[0]), lambda (i,x):i-x):
-            group = map(itemgetter(1), g)
-            idx_range.append([group[0], group[-1]])
+
+        groups = groupby(np.where(power <= power_threshold)[0], key = lambda i,x = count():i-next(x))
+        tmp = [list(g) for k, g in groups]
+
+        for item in tmp:
+            if len(item) == 1:
+                idx_range.append([item[0], item[0]])
+            else:
+                idx_range.append([item[0], item[-1]])
+
+
+        # for k, g in groupby(enumerate(np.where(power <= power_threshold)[0]), key = lambda (i,x):i-x):
+        #     group = map(itemgetter(1), g)
+        #     idx_range.append([group[0], group[-1]])
+
         idx_range = np.array(idx_range)
         silence_time_idx = idx_range[np.where((idx_range[:,1] 
                                                   - idx_range[:,0]) > silence_sec * sec_to_bin), :][0]
@@ -39,13 +52,13 @@ class AudioActDet:
         idx_nums = ()
         for i in silence_time_idx:
             idx_nums = np.append(idx_nums,range(int(i[0]), int(i[1]+1)))
-        idx_act = np.array(list(set(range(power.shape[0])).difference(set(idx_nums))) )
+        idx_act = np.sort(np.array(list(set(range(power.shape[0])).difference(set(idx_nums)))))
         
         silence_time_duration = silence_time_range[:,1] - silence_time_range[:,0]
         silence_time = sum(silence_time_duration)
         return silence_time_idx, silence_time, idx_act
 
-    def detect_silence(self, power_threshold = 0, silence_sec = 0.5, mvg_point = 5):
+    def detect_silence(self, power_threshold = 0, silence_sec = 0.5, mvg_point = 5, PLOT = False, **kwarg):
         # power_threshold is a value that greater than 0. Usually is 1
         # silence_sec_s is the minimum duration of a silence, in seconds. Usually is 0.5.
 
@@ -61,7 +74,18 @@ class AudioActDet:
         hxxf = self.Movingavg(Hxx.sum(axis=0), n = mvg_point)
         lxxf = self.Movingavg(Lxx.sum(axis = 0), n = mvg_point)
         Fxx = lxxf - np.mean(hxxf)
-        # plt.plot(Fxx)
+        if PLOT:
+            plt.figure(figsize = (18,4))
+            if kwarg['tmin'] is None:
+                tmin = self.time_ins.min()
+            if kwarg['tmax'] is None:
+                tmax = tmin + 100
+            t = np.linspace(kwarg['tmin'], kwarg['tmax'], (kwarg['tmax'] - kwarg['tmin']) * self.sec_to_bin + 1)
+            # print(t.shape)
+            # print(Fxx[np.where((self.time_ins >= kwarg['tmin']) & (self.time_ins < kwarg['tmax']))[0]].shape)
+            plt.plot(t, Fxx[np.where((self.time_ins >= kwarg['tmin']) & (self.time_ins < kwarg['tmax']))[0]])
+            plt.hlines(power_threshold, t.min(), t.max(), linestyles = '--', colors = 'r')
+            # plt.xticks
         
         self.silence_seg_2d, _, self.idx_act = self.combine_to_range(Fxx, power_threshold, self.sec_to_bin, silence_sec)
         #si_time_duration = out[1]
